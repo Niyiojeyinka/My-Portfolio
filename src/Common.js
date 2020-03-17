3411161ac879515beb011b7ecc7989e67ff49762
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-03-17 15:18:17 +0100

    deleted unwanted files and added common.js file

--- src/Common.js
+++ src/Common.js
@@ -1,1 +1,15 @@
 import React,{Component} from 'react';
+class Common extends Component {
+constructor(props){
+    super(props);
+}
+
+render() {
+    return (
+       <div>Common</div>
+    );
+}
+
+
+}
+export default Common;
