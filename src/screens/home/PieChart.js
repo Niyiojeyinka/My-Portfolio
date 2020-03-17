4552767fa1c4f32ad8cddac2997c74bbd267e98f
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-03-17 23:54:58 +0100

    added chart to the home screen

---
+++ src/screens/home/PieChart.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Chart from "react-google-charts";
+
+let PieChart =()=>{
+    return (
+
+<Chart
+  width={'100%'}
+  height={'400px'}
+  chartType="PieChart"
+  loader={<div>Loading Chart</div>}
+  data={[
+    ['Specialization', '% of Preference'],
+    ['Backend', 70],
+    ['Frontend', 15],
+    ['Mobile', 15],
+  ]}
+  options={{
+    title: 'Specialization Chart',
+    slices: [
+        {
+          color: "#2BB673"
+        },
+        {
+          color: "#d91e48"
+        },
+        {
+          color: "#007fad"
+        },
+      ],
+
+  }}
+  rootProps={{ 'data-testid': '1' }}
+/>
+
+    )
+}
+export default PieChart;
