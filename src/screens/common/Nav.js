8dbb7ad789eaacb52f48f2bfcad6465ce1c4b3a3
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-03-18 07:56:12 +0100

    worked on nav

--- src/screens/common/Nav.js
+++ src/screens/common/Nav.js
@@ -4,16 +4,19 @@ constructor(props){
     super(props);
 }
 
-render() {
+render(){
     return (
-       <div className="w3-border-bottom w3-border-teal w3-padding-xxlarge">
-          <span className="w3-margin"> Contact me</span>
-           <span className="w3-margin">Articles</span>
-           <span className="w3-margin">Resume</span>
-           <span className="w3-margin ">Portfolio</span>
-          
-       </div>
-    );
+	<header className="w3-bar w3-border-bottom w3-border-teal w3-xlarge w3-cell-row w3-white w3-padding" style={{height:"60px"}}>
+   <div  className="w3-right w3-small  w3-large">
+   <a className="w3-bar-item w3-margin-left w3-padding w3-teal w3-text-white w3-round-jumbo">Home</a> 
+
+   <a className="w3-bar-item w3-margin-left w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo">Resume</a> 
+
+        <a className="w3-bar-item w3-margin-left w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo">Portfolio</a> 
+        <a className="w3-bar-item w3-margin-left w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo">Articles</a> 
+        <a className="w3-bar-item w3-margin-left w3-margin-right w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo">Contact Me</a> 
+
+        </div></header>);
 }
 
 
