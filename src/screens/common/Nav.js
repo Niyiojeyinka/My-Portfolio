267470edae1b83adb74ef65f9a92215dc771eb7a
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-03-18 08:41:26 +0100

    edited nav

--- src/screens/common/Nav.js
+++ src/screens/common/Nav.js
@@ -6,15 +6,14 @@ constructor(props){
 
 render(){
     return (
-	<header className="w3-bar w3-border-bottom w3-border-teal w3-xlarge w3-cell-row w3-white w3-padding" style={{height:"60px"}}>
-   <div  className="w3-right w3-small  w3-large">
+	<header className="w3-bar w3-border-bottom w3-border-teal w3-xlarge w3-white w3-padding w3-top w3-tansparent" style={{height:"60px"}}>
+   <div  className="w3-center w3-small w3-margin-top">
    <a className="w3-bar-item w3-margin-left w3-padding w3-teal w3-text-white w3-round-jumbo">Home</a> 
 
    <a className="w3-bar-item w3-margin-left w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo">Resume</a> 
 
-        <a className="w3-bar-item w3-margin-left w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo">Portfolio</a> 
+        <a className="w3-margin-left w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo">Portfolio</a> 
         <a className="w3-bar-item w3-margin-left w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo">Articles</a> 
-        <a className="w3-bar-item w3-margin-left w3-margin-right w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo">Contact Me</a> 
 
         </div></header>);
 }
