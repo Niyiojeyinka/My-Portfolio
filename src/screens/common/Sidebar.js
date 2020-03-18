03801c706e7981c71ea311852bcd1ff78c11380b
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-03-18 09:14:31 +0100

    reduced all element sizes in sidebar component

--- src/screens/common/Sidebar.js
+++ src/screens/common/Sidebar.js
@@ -16,17 +16,17 @@ render() {
            <div className="w3-text-white w3-bold w3-xxxlarge w3-margin-left w3-margin-top">Olaniyi Philip Ojeyinka</div>
                <img src={profile} className="profile w3-circle" style={{width:"60%"}}/>
            <div className="w3-padding w3-justify profileText">
-<p className="w3-xlarge lead" style={{letterSpacing:"1px"}}>I'm a fullstack Software developer who loves creating solutions that work</p>
+<p className="w3-large lead" style={{letterSpacing:"1px"}}>I'm a fullstack Software developer who loves creating solutions that work</p>
            </div>
-           <span className="w3-xxxlarge w3-margin"><FaGithub/></span>
-           <span className="w3-xxxlarge w3-margin"><FaLinkedinIn/></span>
-           <span className="w3-xxxlarge w3-margin"><FaTwitter/></span>
-           <span className="w3-xxxlarge w3-margin"><IoLogoWhatsapp/></span>
+           <span className="w3-xxlarge w3-margin"><FaGithub/></span>
+           <span className="w3-xxlarge w3-margin"><FaLinkedinIn/></span>
+           <span className="w3-xxlarge w3-margin"><FaTwitter/></span>
+           <span className="w3-xxlarge w3-margin"><IoLogoWhatsapp/></span>
            <hr className="w3-text-black"></hr>
 <div className="w3-padding-large">
 <button className="w3-btn w3-large w3-hide-large" style={{backgroundColor:"rgb(0,150,110)"}}><IoMdDownload/> Download resume</button>
 
-<button className="w3-btn w3-large w3-padding-large w3-hide-small w3-hide-medium" style={{backgroundColor:"rgb(0,150,110)"}}><IoMdDownload/> Download resume</button>
+<button className="w3-btn w3-large w3-padding w3-hide-small w3-hide-medium" style={{backgroundColor:"rgb(0,150,110)"}}><IoMdDownload/> Download resume</button>
 </div>  </div>
     );
 }
