fe9a19f80eeaed18d81262a9c51d6020dce05762
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-03-19 19:30:17 +0100

    added margin left to menu links

--- src/screens/common/Nav.js
+++ src/screens/common/Nav.js
@@ -8,11 +8,11 @@ render(){
     return (
 	<header className="w3-bar w3-border-bottom w3-border-teal w3-xlarge w3-white w3-padding w3-top w3-tansparent" style={{height:"60px"}}>
    <div  className="w3-left w3-small w3-margin-top">
-   <a className="w3-bar-item w3-padding w3-teal w3-text-white w3-round-jumbo">Home</a> 
+   <a className="w3-bar-item w3-padding w3-teal w3-text-white w3-round-jumbo w3-margin-left">Home</a> 
 
-   <a className="w3-bar-item  w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo">Resume</a> 
+   <a className="w3-bar-item  w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo w3-margin-left">Resume</a> 
 
-        <a className="w3-bar-item  w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo">Portfolio</a> 
+        <a className="w3-bar-item  w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo w3-margin-left">Projects</a> 
         <a className="w3-bar-item w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo">Articles</a> 
 
         </div></header>);
