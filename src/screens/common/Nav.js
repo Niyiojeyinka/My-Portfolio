7b9480388e4f457ac24f55ba4eed4838b8a89dee
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-04-05 00:02:33 +0100

    made menu links clickable and more modular

--- src/screens/common/Nav.js
+++ src/screens/common/Nav.js
@@ -4,17 +4,26 @@ import "../../styles/nav.css";
 class Nav extends Component {
 constructor(props){
     super(props);
+    this.state= {
+        menus:[{
+            name:"Home",to:"/"},
+           { name:"Resume",to:"/resume.pdf"},
+           { name:"Projects",to:"/projects"},
+           { name:"Articles",to:"/articles"},
+
+    ]
+    }
 }
 
 render(){
+    let myMenus = this.state.menus.map((eachMenu)=>
+<span  className="w3-bar-item  w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo w3-margin-left"><Link to={`${eachMenu.to}`}>{eachMenu.name}</Link></span>
+
+    );
     return (
 	<header className="w3-bar w3-border-bottom w3-border-teal w3-xlarge w3-white w3-padding w3-top w3-tansparent" style={{height:"60px"}}>
    <div  className="w3-left w3-small w3-margin-top">
-   <span  className="w3-bar-item  w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo w3-margin-left"><Link to="/">Home</Link></span>
-   <span  className="w3-bar-item  w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo w3-margin-left"><Link to="/resume.pdf">Resume</Link></span>
-    <span  className="w3-bar-item  w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo w3-margin-left"><Link to="/projects">Projects</Link></span>
-    <span  className="w3-bar-item  w3-padding w3-hover-teal w3-hover-text-white w3-round-jumbo w3-margin-left"><Link to="/articles">Articles</Link></span>
-
+  {myMenus}
         </div></header>);
 }
 
