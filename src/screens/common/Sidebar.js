68632a45293143d2325e30343e0cf45b74627f06
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-04-04 23:46:13 +0100

    made sidebar links clickable and more modular

--- src/screens/common/Sidebar.js
+++ src/screens/common/Sidebar.js
@@ -8,9 +8,19 @@ import {IoMdDownload} from 'react-icons/io';
 class Sidebar extends Component {
 constructor(props){
     super(props);
+    this.state={ icons:[{link:"https://www.github.com/niyiojeyinka",icon:<FaGithub/>},
+    {link:"https://www.linkedin.com/in/niyiojeyinka",icon:<FaLinkedinIn/>},
+    {link:"https://www.twitter.com/niyiojeyinka",icon:<FaTwitter/>},
+]};
 }
 
 render() {
+    let iconsLink = this.state.icons.map((eachIcon)=>{
+return <span>
+    <a href={`${eachIcon.link}`}> <span className="w3-xxlarge w3-margin">{eachIcon.icon}</span></a>
+</span>
+
+    });
     return (
        <div className="w3-padding-large">
            <br></br><br></br>
@@ -18,10 +28,8 @@ render() {
            <div className="w3-padding w3-justify profileText">
 <p className="w3-large lead" style={{letterSpacing:"1px"}}>I'm a fullstack Software developer who loves creating solutions that work</p>
            </div>
-           <span className="w3-xxlarge w3-margin"><FaGithub/></span>
-           <span className="w3-xxlarge w3-margin"><FaLinkedinIn/></span>
-           <span className="w3-xxlarge w3-margin"><FaTwitter/></span>
-           <hr className="w3-text-black"></hr>
+           {iconsLink}
+          <hr className="w3-text-black"></hr>
 <div className="w3-padding-large">
 <button className="w3-btn w3-large w3-hide-large" style={{backgroundColor:"rgb(0,150,110)"}}><IoMdDownload/> Download resume</button>
 
