7f9edf43e58a01a841b9b46c9d027127a5ea7ead
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-04-06 23:31:16 +0100

    rearranged projects

--- src/screens/home/About.js
+++ src/screens/home/About.js
@@ -5,7 +5,7 @@ let About =()=>{
 return (
 <div className="w3-padding-large w3-center">
     <br></br><br></br>
-    <span className="w3-xxxlarge" style={{color:"rgba(39, 28, 28, 0.74)"}}><strong> I'm Ojeyinka Olaniyi</strong></span>
+    <span className="w3-xxxlarge" style={{color:"rgba(39, 28, 28, 0.74)"}}><strong> I'm Ojeyinka Olaniyi Philip</strong></span>
        <div className="w3-padding-large" style={{letterSpacing:"0.11em"}}><center> <p className="w3-medium w3-justify" style={{textAlign:"center",maxWidth:"60ch"}}>
         A Full-stack developer with more than three years experience developing both backend and frontend of a scalable applications.
         <br></br>Started learning to code around 2014 and by 2016 started working on real-life freelance gigs.
