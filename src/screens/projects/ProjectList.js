b909735e00c7ad11fb382dd586479c3376eedfa8
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-04-17 04:42:15 +0100

    added adnetwork links and web creator links

--- src/screens/projects/ProjectList.js
+++ src/screens/projects/ProjectList.js
@@ -7,11 +7,11 @@ class ProjectList extends Component {
 constructor(props){
   super(props);
   this.state = {projects:[
-    {name:"CBT",description:"A Computer Based Testing Software ideally for schools and colleges(password:user,email:user@gmail.com)",image:"http://niyiojeyinka.byethost7.com/projects/cbt.png",link:"https://niyiojeyinka.heroku.com",github:"https://github.com/Niyiojeyinka/PHPonlineCBT-",stacks:["PHP","MYSQL"]},
-    {name:"Multi school Management",description:"A Website (SAAS) that lets schools create a  school management web software and also a website(password:user,phone:12345678)",image:"http://niyiojeyinka.byethost7.com/projects/getd.png",link:"https://niyiojeyinka.heroku.com",github:"https://github.com/Niyiojeyinka/My-Portfolio.git",stacks:["PHP","MYSQL"]},
-    {name:"Web Creator",description:"An Online theme based web creator and designer that let users create their own website in few clicks(password:user,email:user@gmail.com)",image:"http://niyiojeyinka.byethost7.com/projects/web2.png",link:"https://niyiojeyinka.heroku.com",github:"https://github.com/Niyiojeyinka/My-Portfolio.git",stacks:["PHP","MYSQL"]},
+    {name:"AdNetwork",description:"Advertising Network Server  (password:user,email:user@gmail.com)",image:"https://i.ibb.co/nRL91yj/advertiseradtype.png",link:"http://niyiojeyinka.byethost7.com/ads",github:"https://github.com/Niyiojeyinka/PHP-AdsNetworkServer-",stacks:["PHP","MYSQL"]},
+    {name:"Web Creator",description:"An Online theme based web creator and designer that let users create their own website in few clicks(password:user,email:user@gmail.com)",image:"https://i.ibb.co/zbBxKzV/web2.png",link:"http://niyiojeyinka.byethost7.com/web",github:"https://github.com/Niyiojeyinka/Simple-Theme-Based-Web-builder",stacks:["PHP","MYSQL"]},
+    {name:"CBT",description:"A Computer Based Testing Software ideally for schools and colleges(password:user,email:user@gmail.com)",image:"https://i.ibb.co/Wv0Qz3r/cbt.png",link:"https://niyiojeyinka.heroku.com",github:"https://github.com/Niyiojeyinka/PHPonlineCBT-",stacks:["PHP","MYSQL"]},
+    {name:"Multi school Management",description:"A Website (SAAS) that lets schools create a  school management web software and also a website(password:user,phone:12345678)",image:"https://i.ibb.co/zNdXKwq/getd.png",link:"https://niyiojeyinka.heroku.com",github:"https://github.com/Niyiojeyinka/PHP-Multi-Cloud-School-Management-",stacks:["PHP","MYSQL"]},
     {name:"Pryper Q Bet",description:"A Question Betting Web Apllication that let people bet on Questions from topics they care about (password:user,email:user@gmail.com)",image:"http://niyiojeyinka.byethost7.com/projects/py.png",link:"https://niyiojeyinka.heroku.com",github:"https://github.com/Niyiojeyinka/My-Portfolio.git",stacks:["PHP","MYSQL"]},
-    {name:"AdNetwork",description:"Advertising Network Server  (password:user,email:user@gmail.com)",image:"http://niyiojeyinka.byethost7.com/projects/advertiseradtype.png",link:"http://niyiojeyinka.byethost7.com/ads",github:"https://github.com/Niyiojeyinka/PHP-AdsNetworkServer-",stacks:["PHP","MYSQL"]},
     {name:"Page Chat",description:"A One page Chat  web application written in PHP ,JAVSCRIPT and MYSQL",image:"http://niyiojeyinka.byethost7.com/projects/opc.png",link:"https://niyiojeyinka.heroku.com",github:"https://github.com/Niyiojeyinka/My-Portfolio.git",stacks:["PHP","MYSQL"]},
     {name:"My Portfolio",description:"My portfolio website created with React,Bootstrap CSS framework",image:"http://niyiojeyinka.byethost7.com/projects/portfolio.png",link:"https://niyiojeyinka.heroku.com",github:"https://github.com/Niyiojeyinka/My-Portfolio.git",stacks:["PHP","MYSQL"]},
     {name:"E-voting",description:"An online voting web application software Backend (password:admin,username:admin) login link:{domain}/index.php/officer .",image:"http://niyiojeyinka.byethost7.com/projects/vote.png",link:"https://niyiojeyinka.heroku.com",github:"https://github.com/Niyiojeyinka/My-Portfolio.git",stacks:["PHP","MYSQL"]},
@@ -25,7 +25,7 @@ render(){
     let projects = this.state.projects.map(
         (project,index)=>{
        return <div key={index} className="w3-card-4 w3-margin w3-col l5 m5 s12 w3-display-container">
-           <span className="w3-padding w3-teal w3-bold w3-display-topleft w3-display-hover">{`${project.name}`}</span>
+           <span className="w3-padding w3-https://i.ibb.co/nRL91yj/advertiseradtype.pngteal w3-bold w3-display-topleft w3-display-hover">{`${project.name}`}</span>
            <img src={`${project.image}`} className="" style={{maxHeight:"150px",width:"100%"}}></img>
            <div className="w3-padding w3-small"><p>{`${project.description}`}</p></div>
            <div className="w3-row w3-small">
