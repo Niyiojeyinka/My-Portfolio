8da4ab9172f0f4adf464a846431903ac00fb7df2
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-07-13 08:10:03 +0100

    some data update

--- src/screens/home/About.js
+++ src/screens/home/About.js
@@ -1,22 +1,37 @@
-import React from 'react';
-import { FaGithub} from 'react-icons/fa';
+import React from 'react'
+import { FaGithub } from 'react-icons/fa'
 
-let About =()=>{
-return (
-<div className="w3-padding-large w3-center">
-    <br></br><br></br>
-    <span className="w3-xxxlarge" style={{color:"rgba(39, 28, 28, 0.74)"}}><strong> I'm Ojeyinka Olaniyi Philip</strong></span>
-       <div className="w3-padding-large" style={{letterSpacing:"0.11em"}}><center> <p className="w3-medium w3-justify" style={{textAlign:"center",maxWidth:"60ch"}}>
-        A Full-stack developer with more than three years experience developing both backend and frontend of a scalable applications.
-        <br></br>Started learning to code around 2014 and by 2016 started working on real-life freelance gigs.
-        </p>
-        <a href="https://www.github.com/niyiojeyinka" className="w3-btn w3-large w3-padding w3-margin-left w3-round w3-text-white" style={{backgroundColor:"rgb(0, 150, 110)"}} > <FaGithub/> <span className="w3-small"> Visit Github</span></a>
-
+let About = () => {
+  return (
+    <div className="w3-padding-large w3-center">
+      <br></br>
+      <br></br>
+      <span className="w3-xxxlarge" style={{ color: 'rgba(39, 28, 28, 0.74)' }}>
+        <strong> I'm Ojeyinka Olaniyi Philip</strong>
+      </span>
+      <div className="w3-padding-large" style={{ letterSpacing: '0.11em' }}>
+        <center>
+          {' '}
+          <p
+            className="w3-medium w3-justify"
+            style={{ textAlign: 'center', maxWidth: '60ch' }}
+          >
+            A Full-stack developer with more than three years experience
+            developing both backend and frontend of a scalable applications.
+            <br></br>Started learning to code around 2014 and by 2016 started
+            working on freelance gigs.
+          </p>
+          <a
+            href="https://www.github.com/niyiojeyinka"
+            className="w3-btn w3-large w3-padding w3-margin-left w3-round w3-text-white"
+            style={{ backgroundColor: 'rgb(0, 150, 110)' }}
+          >
+            {' '}
+            <FaGithub /> <span className="w3-small"> Visit Github</span>
+          </a>
         </center>
-
-        </div>
-        </div>
-)
-
+      </div>
+    </div>
+  )
 }
-export default About;
+export default About
--- src/screens/projects/ProjectList.js
+++ src/screens/projects/ProjectList.js
@@ -1,55 +1,158 @@
-import React, {Component} from 'react';
-import { FaGithub} from 'react-icons/fa';
-import { GiClick} from 'react-icons/gi';
-
+import React, { Component } from 'react'
+import { FaGithub } from 'react-icons/fa'
+import { GiClick } from 'react-icons/gi'
 
 class ProjectList extends Component {
-constructor(props){
-  super(props);
-  this.state = {projects:[
-    {name:"AdNetwork",description:"Advertising Network Server  (password:user,email:user@gmail.com)",image:"https://i.ibb.co/nRL91yj/advertiseradtype.png",link:"https://web-web20.000webhostapp.com/ads",github:"https://github.com/Niyiojeyinka/PHP-AdsNetworkServer-",stacks:["PHP","MYSQL"]},
-    {name:"Web Creator",description:"An Online theme based web creator and designer that let users create their own website in few clicks(password:user,email:user@gmail.com)",image:"https://i.ibb.co/zbBxKzV/web2.png",link:"https://web-web20.000webhostapp.com/web",github:"https://github.com/Niyiojeyinka/Simple-Theme-Based-Web-builder",stacks:["PHP","MYSQL"]},
-    {name:"CBT",description:"A Computer Based Testing Software ideally for schools and colleges(password:user,email:user@gmail.com)",image:"https://i.ibb.co/Wv0Qz3r/cbt.png",link:"https://web-web20.000webhostapp.com/cbt",github:"https://github.com/Niyiojeyinka/PHPonlineCBT-",stacks:["PHP","MYSQL"]},
-    {name:"Multi school Management",description:"A Website (SAAS) that lets schools create a  school management web software and also a website(password:user,phone:12345678)",image:"https://i.ibb.co/zNdXKwq/getd.png",link:"https://web-web20.000webhostapp.com/csm",github:"https://github.com/Niyiojeyinka/PHP-Multi-Cloud-School-Management-",stacks:["PHP","MYSQL"]},
-    {name:"Pryper Q Bet",description:"A Question Betting Web Apllication that let people bet on Questions from topics they care about (password:user,email:user@gmail.com)",image:"https://i.ibb.co/fHbhZCc/py.png",link:"https://web-web20.000webhostapp.com/bet",github:"https://github.com/Niyiojeyinka/My-Portfolio.git",stacks:["PHP","MYSQL"]},
-    {name:"E-voting",description:"An online voting web application software Backend (password:admin,username:admin) login link:{domain}/index.php/officer .",image:"https://i.ibb.co/mB9Pp9B/vote.png",link:"https://web-web20.000webhostapp.com/vote",github:"https://github.com/Niyiojeyinka/My-Portfolio.git",stacks:["PHP","MYSQL"]},
-    {name:"Business Listing",description:"A Classic Business directory website url/admin (password:admin,email:admin@gmail.com)",image:"https://i.ibb.co/DbgT2Pq/listing.png",link:"http://cbdsonline.herokuapp.com",github:"https://github.com/Niyiojeyinka/Business-Listing.git",stacks:["PHP","MYSQL"]},
-    {name:"My Portfolio",description:"My portfolio website created with React,Bootstrap CSS framework",image:"https://i.ibb.co/6sfj4Fc/portfolio.png",link:"https://niyiojeyinka.heroku.com",github:"https://github.com/Niyiojeyinka/My-Portfolio.git",stacks:["PHP","MYSQL"]},
-    {name:"Page Chat",description:"A One page Chat  web application written in PHP ,JAVSCRIPT and MYSQL",image:"https://i.ibb.co/Xjm81j2/opc.png",link:"https://niyiojeyinka.heroku.com",github:"https://github.com/Niyiojeyinka/My-Portfolio.git",stacks:["PHP","MYSQL"]},
-    {name:"Social Media",description:"A Social Media web application for users to meet and chat.",image:"https://i.ibb.co/dKHfnVk/sm.png",link:"https://niyiojeyinka.heroku.com",github:"https://github.com/Niyiojeyinka/My-Portfolio.git",stacks:["PHP","MYSQL"]},
-    {name:"CSV Tools",description:"A developer PHP class to generate a CSV file.",image:"https://i.ibb.co/zHN0444/csv.png",stacks:["PHP","MYSQL"]},
-
-]};
+  constructor(props) {
+    super(props)
+    this.state = {
+      projects: [
+        {
+          name: 'AdNetwork',
+          description:
+            'Advertising Network Server  (password:user,email:user@gmail.com)',
+          image: 'https://i.ibb.co/nRL91yj/advertiseradtype.png',
+          link: 'https://web-web20.000webhostapp.com/ads',
+          github: 'https://github.com/Niyiojeyinka/Adware',
+          stacks: ['PHP', 'MYSQL'],
+        },
+        {
+          name: 'Web Creator',
+          description:
+            'An Online theme based web creator and designer that let users create their own website in few clicks(password:user,email:user@gmail.com)',
+          image: 'https://i.ibb.co/zbBxKzV/web2.png',
+          link: 'https://web-web20.000webhostapp.com/web',
+          github:
+            'https://github.com/Niyiojeyinka/Simple-Theme-Based-Web-builder',
+          stacks: ['PHP', 'MYSQL'],
+        },
+        {
+          name: 'CBT',
+          description:
+            'A Computer Based Testing Software ideally for schools and colleges(password:user,email:user@gmail.com)',
+          image: 'https://i.ibb.co/Wv0Qz3r/cbt.png',
+          link: 'https://web-web20.000webhostapp.com/cbt',
+          github: 'https://github.com/Niyiojeyinka/PHPonlineCBT-',
+          stacks: ['PHP', 'MYSQL'],
+        },
+        {
+          name: 'Multi school Management',
+          description:
+            'A Website (SAAS) that lets schools create a  school management web software and also a website(password:user,phone:12345678)',
+          image: 'https://i.ibb.co/zNdXKwq/getd.png',
+          link: 'https://web-web20.000webhostapp.com/csm',
+          github:
+            'https://github.com/Niyiojeyinka/PHP-Multi-Cloud-School-Management-',
+          stacks: ['PHP', 'MYSQL'],
+        },
+        {
+          name: 'Pryper Q Bet',
+          description:
+            'A Question Betting Web Apllication that let people bet on Questions from topics they care about (password:user,email:user@gmail.com)',
+          image: 'https://i.ibb.co/fHbhZCc/py.png',
+          link: 'https://web-web20.000webhostapp.com/bet',
+          github: 'https://github.com/Niyiojeyinka/My-Portfolio.git',
+          stacks: ['PHP', 'MYSQL'],
+        },
+        {
+          name: 'E-voting',
+          description:
+            'An online voting web application software Backend (password:admin,username:admin) login link:{domain}/index.php/officer .',
+          image: 'https://i.ibb.co/mB9Pp9B/vote.png',
+          link: 'https://web-web20.000webhostapp.com/vote',
+          github: 'https://github.com/Niyiojeyinka/My-Portfolio.git',
+          stacks: ['PHP', 'MYSQL'],
+        },
+        {
+          name: 'Business Listing',
+          description:
+            'A Classic Business directory website url/admin (password:admin,email:admin@gmail.com)',
+          image: 'https://i.ibb.co/DbgT2Pq/listing.png',
+          link: 'http://cbdsonline.herokuapp.com',
+          github: 'https://github.com/Niyiojeyinka/Business-Listing.git',
+          stacks: ['PHP', 'MYSQL'],
+        },
+        {
+          name: 'My Portfolio',
+          description:
+            'My portfolio website created with React,Bootstrap CSS framework',
+          image: 'https://i.ibb.co/6sfj4Fc/portfolio.png',
+          link: 'https://niyiojeyinka.heroku.com',
+          github: 'https://github.com/Niyiojeyinka/My-Portfolio.git',
+          stacks: ['PHP', 'MYSQL'],
+        },
+        {
+          name: 'Page Chat',
+          description:
+            'A One page Chat  web application written in PHP ,JAVSCRIPT and MYSQL',
+          image: 'https://i.ibb.co/Xjm81j2/opc.png',
+          link: 'https://niyiojeyinka.heroku.com',
+          github: 'https://github.com/Niyiojeyinka/My-Portfolio.git',
+          stacks: ['PHP', 'MYSQL'],
+        },
+        {
+          name: 'Social Media',
+          description:
+            'A Social Media web application for users to meet and chat.',
+          image: 'https://i.ibb.co/dKHfnVk/sm.png',
+          link: 'https://niyiojeyinka.heroku.com',
+          github: 'https://github.com/Niyiojeyinka/My-Portfolio.git',
+          stacks: ['PHP', 'MYSQL'],
+        },
+        {
+          name: 'CSV Tools',
+          description: 'A developer PHP class to generate a CSV file.',
+          image: 'https://i.ibb.co/zHN0444/csv.png',
+          stacks: ['PHP', 'MYSQL'],
+        },
+      ],
+    }
+  }
+  render() {
+    let projects = this.state.projects.map((project, index) => {
+      return (
+        <div
+          key={index}
+          className="w3-card-4 w3-margin w3-col l5 m5 s12 w3-display-container"
+        >
+          <span className="w3-padding w3-teal w3-bold w3-display-topleft w3-display-hover">{`${project.name}`}</span>
+          <img
+            src={`${project.image}`}
+            alt={project.name}
+            className=""
+            style={{ maxHeight: '150px', width: '100%' }}
+          ></img>
+          <div className="w3-padding w3-small">
+            <p>{`${project.description}`}</p>
+          </div>
+          <div className="w3-row w3-small">
+            <div className="w3-col l6 m6 s6 w3-padding-small w3-center w3-border-right w3-border-gray">
+              <a
+                href={`${project.github}`}
+                className="w3-button w3-hover-text-black"
+              >
+                View source{' '}
+                <span className="w3-small w3-margin-bottom">
+                  <FaGithub />
+                </span>
+              </a>
+            </div>
+            <div className="w3-col l6 m6 s6 w3-padding-small w3-center w3-border-left w3-border-gray">
+              <a
+                href={`${project.link}`}
+                className="w3-button w3-hover-text-black"
+              >
+                Live/Demo{' '}
+                <span className="w3-small w3-margin-bottom">
+                  <GiClick />
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      )
+    })
+    return <div className="w3-row w3-margin-top w3-padding-top">{projects}</div>
+  }
 }
-render(){
-    let projects = this.state.projects.map(
-        (project,index)=>{
-       return <div key={index} className="w3-card-4 w3-margin w3-col l5 m5 s12 w3-display-container">
-           <span className="w3-padding w3-teal w3-bold w3-display-topleft w3-display-hover">{`${project.name}`}</span>
-           <img src={`${project.image}`} alt={project.name} className="" style={{maxHeight:"150px",width:"100%"}}></img>
-           <div className="w3-padding w3-small"><p>{`${project.description}`}</p></div>
-           <div className="w3-row w3-small">
-<div className='w3-col l6 m6 s6 w3-padding-small w3-center w3-border-right w3-border-gray'>
-   <a href={`${project.github}`} className="w3-button w3-hover-text-black">View source <span className="w3-small w3-margin-bottom"><FaGithub/></span></a>  
-</div>
-<div className='w3-col l6 m6 s6 w3-padding-small w3-center w3-border-left w3-border-gray'>
-<a href={`${project.link}`} className="w3-button w3-hover-text-black">Live/Demo <span className="w3-small w3-margin-bottom"><GiClick/></span></a>
-</div>
-
-           </div>
-
-       </div>  
-
-        }
-    );
-    return (
-     <div className="w3-row w3-margin-top w3-padding-top">
-        {projects}
-
-     </div>
-
-    );
-}
-
-}
-export default ProjectList;
+export default ProjectList
